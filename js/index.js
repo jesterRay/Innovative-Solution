97a2719992226f63ae90138ac9bdd641a4f26161
jesterRay <110118157+jesterRay@users.noreply.github.com>
2023-08-07 13:57:59 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+
+function main(){
+    console.log(window.innerWidth,window.innerHeight);
+
+}
+
+
+
+
+
+
+//run when dom is fully loaded
+$(document).ready(main());
---
+++ js/scroll.js
@@ -0,0 +1,34 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+let pages = gsap.utils.toArray(".pages");
+
+let pagesTop = pages.map(page =>
+    ScrollTrigger.create({
+        trigger:page,
+        start: "top top"
+    })
+);
+
+pages.forEach((page,i) => {
+    ScrollTrigger.create({
+        trigger: page,
+        start: () => page.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
+        pin: true,
+        pinSpacing: false
+    });
+});
+
+
+
+ScrollTrigger.create({
+    snap: {
+        snapTo: (progress, self) => {
+            let tops = pagesTop.map(st => st.start),
+            snapScroll = gsap.utils.snap(tops, self.scroll());
+
+            return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window),snapScroll);
+        },
+        duration:0.5      
+    }
+});
