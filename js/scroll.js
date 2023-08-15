a220087f2a12f7af4df85bb77a79b8b6933c2a8a
Sanan Abid <sananabid174@gmail.com>
2023-08-15 17:56:16 +0500

    scoll animation till 992px

--- js/scroll.js
+++ js/scroll.js
@@ -1,55 +1,61 @@
+
+
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
+
 let pages = gsap.utils.toArray(".pages");
-let pagesTop = pages.map(page =>
-    ScrollTrigger.create({
-        trigger: page,
-        start: "top top"
-    })
-);
 
-pages.forEach((page, i) => {
-    ScrollTrigger.create({
-        trigger: page,
-        start: () => page.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
-        pin: true,
-        pinSpacing: false
-    });
-});
 
-let snapScrollTrigger;
 
-function createSnapScrollTrigger() {
-    snapScrollTrigger = ScrollTrigger.create({
-        snap: {
-            snapTo: (progress, self) => {
-                let tops = pagesTop.map(st => st.start),
-                snapScroll = gsap.utils.snap(tops, self.scroll());
-
-                return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll);
-            },
-            duration: 0.5
-        }
-    });
-}
-
-// Call the function initially
-createSnapScrollTrigger();
-
-// Function to check screen width and enable/disable ScrollTrigger animations
+// Function to check the screen width
 function checkScreenWidth() {
-    if (window.innerWidth < 768) {
-        // Disable ScrollTrigger animations
-        snapScrollTrigger.kill(); // Remove the snap ScrollTrigger functionality
-        snapScrollTrigger = null; // Reset the reference
+    const screenWidth = window.innerWidth;
+    
+    if (screenWidth < 992) {
+      // Screen width is less than 768 pixels
+      console.log("no animation");
+      // Perform actions specific to small screens
     } else {
-        // Enable ScrollTrigger animations
-        createSnapScrollTrigger();
+        let pagesTop = pages.map(page =>
+            ScrollTrigger.create({
+                trigger:page,
+                start: "top top"
+            })
+        );
+        
+        pages.forEach((page,i) => {
+            ScrollTrigger.create({
+                trigger: page,
+                start: () => page.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
+                pin: true,
+                pinSpacing: false
+            });
+        });
+        
+        
+        
+        ScrollTrigger.create({
+            snap: {
+                snapTo: (progress, self) => {
+                    let tops = pagesTop.map(st => st.start),
+                    snapScroll = gsap.utils.snap(tops, self.scroll());
+        
+                    return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window),snapScroll);
+                },
+                duration:0.5      
+            }
+        });
+        
+        
     }
-}
-
-// Check screen width on initial page load
-checkScreenWidth();
-
-// Listen for window resize events
-window.addEventListener('resize', checkScreenWidth);
+  }
+  
+  // Call the function initially
+  checkScreenWidth();
+  
+  // Listen for window resize events
+  window.addEventListener('resize', checkScreenWidth);
+  
