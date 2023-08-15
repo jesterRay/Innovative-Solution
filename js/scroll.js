31f0845b77e1cbb0352034c74cc5cde945574de9
Sanan Abid <sananabid174@gmail.com>
2023-08-15 17:06:56 +0500

    done with final animation going for approval

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
 let pages = gsap.utils.toArray(".pages");
-
 let pagesTop = pages.map(page =>
     ScrollTrigger.create({
-        trigger:page,
+        trigger: page,
         start: "top top"
     })
 );
 
-pages.forEach((page,i) => {
+pages.forEach((page, i) => {
     ScrollTrigger.create({
         trigger: page,
         start: () => page.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
@@ -19,16 +17,39 @@ pages.forEach((page,i) => {
     });
 });
 
+let snapScrollTrigger;
 
+function createSnapScrollTrigger() {
+    snapScrollTrigger = ScrollTrigger.create({
+        snap: {
+            snapTo: (progress, self) => {
+                let tops = pagesTop.map(st => st.start),
+                snapScroll = gsap.utils.snap(tops, self.scroll());
 
-// ScrollTrigger.create({
-//     snap: {
-//         snapTo: (progress, self) => {
-//             let tops = pagesTop.map(st => st.start),
-//             snapScroll = gsap.utils.snap(tops, self.scroll());
+                return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll);
+            },
+            duration: 0.5
+        }
+    });
+}
 
-//             return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window),snapScroll);
-//         },
-//         duration:0.5      
-//     }
-// });
+// Call the function initially
+createSnapScrollTrigger();
+
+// Function to check screen width and enable/disable ScrollTrigger animations
+function checkScreenWidth() {
+    if (window.innerWidth < 768) {
+        // Disable ScrollTrigger animations
+        snapScrollTrigger.kill(); // Remove the snap ScrollTrigger functionality
+        snapScrollTrigger = null; // Reset the reference
+    } else {
+        // Enable ScrollTrigger animations
+        createSnapScrollTrigger();
+    }
+}
+
+// Check screen width on initial page load
+checkScreenWidth();
+
+// Listen for window resize events
+window.addEventListener('resize', checkScreenWidth);
